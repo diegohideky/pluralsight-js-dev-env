3d4d3592df6a36515349de9ca1b3bf8419380b0f
Diego Hideky <diegohideky@gmail.com>
2018-02-08 16:48:01 -0200

    Bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,15 +1,21 @@
 import express from 'express';
-import path    from 'path'
+import path    from 'path';
 import open    from 'open';
+import webpack from 'webpack';
+import config  from '../webpack.config.dev';
 
-var port = 3000
-  , app  = express();
+const port = 3000
+  , app  = express()
+  , compiler = webpack(config);
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-app.listen(port, function(err) {
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+
+app.listen(port, err => {
   if (err) {
     console.log(err);
   } else {
