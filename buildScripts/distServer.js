48852865d0592144e6427671f141c17d38058d96
Diego Hideky <diegohideky@gmail.com>
2018-03-02 22:08:25 -0300

    Production Deploy

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -13,15 +13,6 @@ app.use(express.static('dist'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
 
-app.get('/users', (req, res) => {
-  // Hard coding for simplicity. Pretend this hits a real database
-  res.json([
-    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
-    {"id": 2, "firstName": "Tammy", "lastName": "Northon", "email": "tnorthon@yahoo.com"},
-    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
-  ]);
-});
-
 app.listen(port, err => {
   if (err) {
     console.log(err);
